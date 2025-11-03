ad2e17f2f7f31eabe9cc5599d7904afb65a59287
Adnan Shaikh <github@adnanshaikh.com>
2025-11-02 21:51:16 -0800

    disable git signing

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -16,15 +16,15 @@
       enable = true;
       userName = vars.fullName;
       inherit (vars) userEmail;
-      extraConfig = {
-        commit.gpgsign = true;
-        gpg.format = "ssh";
-        gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
-        user.signingkey =
-          if osConfig.networking.hostName == "workchng"
-          then vars.sshPublicKeyWork
-          else vars.sshPublicKeyPersonal;
-      };
+      #extraConfig = {
+      #  commit.gpgsign = true;
+      #  gpg.format = "ssh";
+      #  gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+      #  user.signingkey =
+      #    if osConfig.networking.hostName == "workchng"
+      #    then vars.sshPublicKeyWork
+      #    else vars.sshPublicKeyPersonal;
+      #};
     };
   };
 }
