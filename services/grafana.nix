06f6a426e7069e6a1116f43190913de1e30ed91d
Adnan Shaikh <github@adnanshaikh.com>
2026-02-17 20:11:36 -0800

    try

--- services/grafana.nix
+++ services/grafana.nix
@@ -15,13 +15,26 @@
       server = {
         http_addr = "127.0.0.1";
         http_port = 3000;
-        root_url = "https://grafana.adnanshaikh.com";
+        root_url = "https://grafana.adnanshaikh.com/";
         domain = "grafana.adnanshaikh.com";
       };
       security = {
         cookie_secure = true;
       };
       analytics.reporting_enabled = false;
+    };
+  };
+
+  services.nginx = {
+    virtualHosts = {
+      "grafana.adnanshaikh.com" = {
+        forceSSL = true;
+        useACMEHost = "adnanshaikh.com";
+        locations."/" = {
+          recommendedProxySettings = true;
+          proxyPass = "http://127.0.0.1:3000/";
+        };
+      };
     };
   };
 
