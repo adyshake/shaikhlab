9a34b9da51a4f3b4c6344f3642d30b715b787da4
Adnan Shaikh <github@adnanshaikh.com>
2026-04-07 20:59:07 -0700

    fix

--- services/zwave-js-ui.nix
+++ services/zwave-js-ui.nix
@@ -4,10 +4,10 @@
     ./_nginx.nix
   ];
 
-  # Zooz 800 Z-Wave stick. serialPort = DeviceAllow only; ZWAVE_PORT actually enables the driver.
+  # Zooz 800 Z-Wave stick (see /dev/serial/by-id/ on svr1shaikh).
   services.zwave-js-ui = {
     enable = true;
-    serialPort = "/dev/serial/by-id/usb-Zooz_800_Z-Wave_Stick_533D004242-if00";;
+    serialPort = "/dev/serial/by-id/usb-Zooz_800_Z-Wave_Stick_533D004242-if00";
     settings = {
       HOST = "127.0.0.1";
       PORT = "8091";
