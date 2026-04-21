53511fc82a1395bb6bafe3116dd7c81e4d6768d3
Adnan Shaikh <github@adnanshaikh.com>
2026-04-20 18:36:56 -0700

    fix ventoy

--- modules/nixos/_packages.nix
+++ modules/nixos/_packages.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   environment.systemPackages = with pkgs; [
     efibootmgr
     git
@@ -8,9 +12,10 @@
     vim
   ];
 
-  # temp
+  # Ventoy is marked insecure in nixpkgs due to binary-blob concerns; we accept
+  # the risk since it's only used to flash NixOS installer USBs from the host.
+  # Using the predicate form instead of a pinned version string so routine
+  # nixpkgs bumps of ventoy don't keep breaking rebuilds.
   # inspo: https://github.com/ventoy/Ventoy/issues/3224
-  nixpkgs.config.permittedInsecurePackages = [
-    "ventoy-1.1.07"
-  ];
+  nixpkgs.config.allowInsecurePredicate = pkg: lib.getName pkg == "ventoy";
 }
