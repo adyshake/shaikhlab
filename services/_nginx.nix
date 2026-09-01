2bd6916a51ef865ed6d48fd891073f2390598c3c
Adnan Shaikh <github@adnanshaikh.com>
2026-08-31 20:26:34 -0700

    Return 404 for unknown nginx Host headers.
    Unknown names were falling through to Forgejo as the first TLS vhost.

--- services/_nginx.nix
+++ services/_nginx.nix
@@ -1,9 +1,20 @@
 {
+  imports = [./_acme.nix];
+
   services.nginx = {
     enable = true;
     recommendedTlsSettings = true;
     recommendedOptimisation = true;
     recommendedGzipSettings = true;
+
+    # First matching TLS server was git.adnanshaikh.com, so unknown Host
+    # headers (beancount after Fava was removed, typos, probes) served Forgejo.
+    virtualHosts."_" = {
+      default = true;
+      addSSL = true;
+      useACMEHost = "adnanshaikh.com";
+      locations."/".return = "404";
+    };
   };
 
   # nginx terminates every vhost, including jail origins. Cap it so a
